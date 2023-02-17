0bd18341ec7d4075104dc1e594e99ca49b0081aa
Dhruvil Gelani <dhruvilgelani96@gmail.com>
2023-02-17 16:01:09 +0530

    Recent

--- abc.java
+++ abc.java
@@ -1,5 +1,6 @@
 class abc{
     public static void main(String[] args) {
-        System.out.println("Hello World! New");
+        System.out.println("Hello World!");
+        System.out.println("NEW");
     }
 }
